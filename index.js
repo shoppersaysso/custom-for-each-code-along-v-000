c8e97c6959b35563f86bcdf30b4b8c82597ff341
Sara Hopper <sara.hopper@gmail.com>
2017-08-29 17:53:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const dogs = [
-  { name: "Fido", age: 2 },
-  { name: "Odie", age: 8 },
-  { name: "Ralph", age: 4 }
-];
-
-for (let i = 0, l = dogs.length; i < 1; i++) {
-  console.log(i);
-}
+function forEach(iterable, callback) {}
